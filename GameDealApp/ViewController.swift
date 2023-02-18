cc7e8f047beb08bbbace5ab1ab3f6741ba56c254
Thiago <thiagos2000@gmail.com>
2023-02-18 00:40:37 -0300

    first commit

---
+++ GameDealApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameDealApp
+//
+//  Created by Thiago de Oliveira Sousa on 18/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
